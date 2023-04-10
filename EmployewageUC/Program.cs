0949dffa0c2e15f464fc1ab7aa081552c7a9a058
aarya200 <aaryakhanapurkar1@gmail.com>
2023-04-10 16:33:46 +0530

    welcome message

---
+++ EmployewageUC/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //welcome message
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+        }
+    }
+}
