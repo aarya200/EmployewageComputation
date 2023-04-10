7ab90285e639f80fa4d9f74993f81e304907b351
aarya200 <aaryakhanapurkar1@gmail.com>
2023-04-10 21:42:46 +0530

    checking attendence

--- EmployewageUC/Program.cs
+++ EmployewageUC/Program.cs
@@ -7,8 +7,22 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
+            int isPresent = 1;
             //welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            //UC1-EmployeeAttendance
+            Random random = new Random();
+            int employeeAttendance = random.Next(0, 2); //0 or 1
+            if (employeeAttendance == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
         }
     }
 }
+
